a37f5113dc8bb50515138388e029c84c4cbf5631
Eugene Kotov <mrkev2112@mail.ru>
2020-11-16 23:56:43 +0700

    Create likedList component

---
+++ src/components/LikedList.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <v-expansion-panels
+    color="secondary"
+    class="mx-auto rounded-lg"
+    max-width="200"
+  >
+  <h3>Liked History:</h3>
+    <v-expansion-panel v-for="likedWeather in likedList" :key="likedWeather.id">
+      <v-expansion-panel-header>
+        {{likedWeather.name}}
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <WeatherCard :weather="likedWeather" />
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<script>
+import WeatherCard from "@/components/WeatherCard";
+export default {
+  components: {
+    WeatherCard
+  },
+  props: {
+    likedList: {
+      type: Array
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped></style>
