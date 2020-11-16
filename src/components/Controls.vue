e7206e9ce68fea1d6196000149df04f3ca342f8a
Eugene Kotov <mrkev2112@mail.ru>
2020-11-16 23:55:20 +0700

    Create controls component

---
+++ src/components/Controls.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <v-card-actions>
+    <v-btn class="mx-2" fab dark x-large color="primary" @click="nextWeather(false)">
+      <v-icon dark>
+        mdi-thumb-down
+      </v-icon>
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn class="mx-2" fab dark x-large color="primary" @click="nextWeather(weather)">
+      <v-icon dark>
+        mdi-thumb-up
+      </v-icon>
+    </v-btn>
+  </v-card-actions>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  methods: {
+    ...mapActions(['nextWeather'])
+  },
+  props: {
+    weather: {
+      type: Object
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped></style>
