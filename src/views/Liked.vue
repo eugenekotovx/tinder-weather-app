1eb15e8a24410971af91a7d5d7120b596dedc410
Eugene Kotov <mrkev2112@mail.ru>
2020-11-17 23:56:34 +0700

    Rename: 'about' -> 'Liked'

---
+++ src/views/Liked.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+
+    <v-row align="center">
+      <v-col class="display-3 text-center">
+        <LikedList :likedList="likedHistory" />
+      </v-col>
+    </v-row>
+</template>
+
+<script>
+import LikedList from "@/components/LikedList";
+import { mapState } from "vuex";
+export default {
+  components: {
+    LikedList
+  },
+  computed: {
+    ...mapState(["likedHistory"])
+  }
+};
+</script>
+
+<style lang="css" scoped></style>
