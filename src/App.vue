3c39fa9d7b93c851a6cb83c5291db0426b094e27
Eugene Kotov <mrkev2112@mail.ru>
2020-11-19 16:38:19 +0700

    Add theme switcher

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,64 @@
 <template>
-  <v-app :style="{'background': $vuetify.theme.themes.dark.background}">
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-item-group class="d-flex align-center white--text">
+        Theme:
+        <v-switch
+          v-model="$vuetify.theme.dark"
+          inset
+          hide-details
+          :append-icon="
+            $vuetify.theme.dark ? 'mdi-weather-pouring' : 'mdi-weather-hazy'
+          "
+          color="accent"
+          class="mr-4 ml-4"
+          :label="$vuetify.theme.dark ? 'rainy' : 'sunny'"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      </v-item-group>
       <v-spacer></v-spacer>
-      <router-link tag="div" :to="{path: '/'}"> <v-btn> home </v-btn> </router-link>
-
+      <v-item-group class="d-flex">
+        <router-link tag="div" :to="{ path: '/' }">
+          <v-btn class="accent--text secondary mr-10">
+            <v-icon class="mr-4">mdi-home</v-icon> home
+          </v-btn>
+        </router-link>
+        <router-link tag="div" :to="{ path: '/liked' }">
+          <v-btn class="accent--text secondary"
+            ><v-icon class="mr-4">mdi-heart</v-icon> liked
+          </v-btn>
+        </router-link>
+      </v-item-group>
     </v-app-bar>
 
     <v-main>
-      <v-container fluid>
-      <router-view></router-view>
+      <v-container>
+        <router-view></router-view>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
-  components: {
+  components: {},
+  computed: {
+    theme() {
+      return this.$vuetify.theme.dark ? "dark" : "light";
+    }
   },
-
   data: () => ({
     //
-  }),
+  })
 };
 </script>
+
+<style media="screen">
+.theme--dark.v-application {
+  background-color: var(--v-background-base, #455a64) !important;
+}
+.theme--light.v-application {
+  background-color: var(--v-background-base, #FFFACC) !important;
+}
+</style>
