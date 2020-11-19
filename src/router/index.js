ab7cd16e400f45b02a4f9a81c8a7fb7b6fee4b7e
Eugene Kotov <mrkev2112@mail.ru>
2020-11-19 16:37:43 +0700

    Rename 'about' to 'liked'

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,11 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/liked',
+    name: 'Liked',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "liked" */ '../views/Liked.vue')
   }
 ]
 
