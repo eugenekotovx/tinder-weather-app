549da7ba9ae664a8c28ba80b70dc064084e14038
Eugene Kotov <mrkev2112@mail.ru>
2020-12-04 23:45:27 +0700

    Add new request for 404 error

--- src/service/WeatherApiService.js
+++ src/service/WeatherApiService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
-import cities from 'cities.json';
-const apiKey = '3946ccfbd734fa06e00d0ba21ac89d28'
+import cities from "cities.json";
+import getRandomInt from "@/helpers/helpers.js";
+
+const apiKey = "3946ccfbd734fa06e00d0ba21ac89d28";
 
 const weatherApi = axios.create({
   baseURL: `http://api.openweathermap.org/data/2.5`,
@@ -12,23 +14,36 @@ const weatherApi = axios.create({
   timeout: 10000
 });
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
 const randomCity = () => {
-  let num = getRandomInt(128769)
-  return cities[num].name
-}
+  //get a random city index
+  let num = getRandomInt(128769);
+  // get a random city name by a random index from cities.json
+  return cities[num].name;
+};
 
 export default {
   getWeather() {
-    return weatherApi.get('/weather?q=' + randomCity() + '&units=metric&appid=' + apiKey)
-    .then(data => {
-      return data
-    })
-    .catch(error => {
-      console.log(error)
-    })
+    return (
+      weatherApi
+        // get cities from api using random name
+        .get("/weather?q=" + randomCity() + "&units=metric&appid=" + apiKey)
+        .then(data => {
+          return data;
+        })
+        .catch(error => {
+          // Often the cities do not coincide by name from mock api, so we make a request again
+          if (error.response.status == 404) {
+            return weatherApi
+              .get(
+                "/weather?q=" + randomCity() + "&units=metric&appid=" + apiKey
+              )
+              .then(data => {
+                return data;
+              });
+          } else {
+            console.log(error.response);
+          }
+        })
+    );
   }
 };
