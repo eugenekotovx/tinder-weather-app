f1967fb01ee19ad88f7e51c16606cd20cbe8810c
Eugene Kotov <mrkev2112@mail.ru>
2020-12-04 23:48:07 +0700

    Refactor code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,33 @@
 <template>
-  <WeatherCard class="mt-4" :weather="currentWeather">
-    <Controls :weather="currentWeather" />
+  <WeatherCard class="mt-4" :weather="currentWeather" :VerdictControls="true">
+    <VerdictControls :weather="currentWeather" />
   </WeatherCard>
 </template>
 
 <script>
+import store from "@/store/index.js";
 import WeatherCard from "@/components/WeatherCard";
-import Controls from "@/components/Controls";
+import VerdictControls from "@/components/VerdictControls";
 import { mapState, mapGetters } from "vuex";
 export default {
   name: "Home",
   components: {
     WeatherCard,
-    Controls
+    VerdictControls
   },
-  mounted() {
-    if (!this.$store.state.weatherList.length) {
-      this.$store.dispatch("loadApp");
+  created() {
+    store.dispatch("loadNewWeather");
+  },
+  updated() {
+    if (store.state.weatherList.length < 5) {
+      store.dispatch("loadNewWeather");
     }
   },
   computed: {
     ...mapState(["weatherList"]),
     ...mapGetters(["currentWeather"])
-  },
-  methods: {}
+  }
 };
 </script>
+
+<style lang="scss" scoped></style>
