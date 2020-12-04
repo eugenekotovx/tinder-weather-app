14eadb16d86b0d11d4f7717cf0d194338acbe65c
Eugene Kotov <mrkev2112@mail.ru>
2020-12-04 23:47:45 +0700

    Refactor code

--- src/store/index.js
+++ src/store/index.js
@@ -15,55 +15,51 @@ export default new Vuex.Store({
       state.weatherList.push(weather);
     },
     NEXT_WEATHER(state) {
-      state.weatherList.shift()
+      state.weatherList.shift();
     },
     ADD_TO_LIKED(state, weather) {
-      state.likedHistory.push(weather)
+      state.likedHistory.push(weather);
     },
     UPD_VERDICT(state, verdict) {
       state.lastVerdict = verdict;
     },
+    REMOVE_FROM_LIKED_HISTORY(state, weatherFromLikedList) {
+      state.likedHistory.splice(weatherFromLikedList, 1);
+    },
+    UPD_HISTORY(state, weather) {
+      let weatherInHistory = state.likedHistory.indexOf(weather);
+      Vue.set(state.likedHistory, weatherInHistory, weather);
+    }
   },
   actions: {
     setWeather({ commit }, weather) {
+      Vue.set(weather, "comment", "");
       commit("SET_WEATHER", weather);
     },
-    async loadApp({ dispatch}) {
-      await dispatch('loadWeatherList')
-    },
-    async nextWeather({dispatch, commit}, weather) {
-      await commit("NEXT_WEATHER")
-      await dispatch("loadNewWeather")
+    nextWeather({ commit }, weather) {
       if (weather) {
-       await commit("ADD_TO_LIKED", weather)
+        commit("ADD_TO_LIKED", weather);
+        commit("UPD_VERDICT", true);
+      } else {
+        commit("UPD_VERDICT", false);
       }
     },
-    addToLiked({commit}, weather) {
-      commit("ADD_TO_LIKED", weather)
+    addToLiked({ commit }, weather) {
+      commit("ADD_TO_LIKED", weather);
+    },
     refreshVerdict({ commit }) {
       commit("UPD_VERDICT", "");
       commit("NEXT_WEATHER");
     },
-    async loadNewWeather({dispatch}) {
-      await WeatherApi.getWeather()
+    loadNewWeather({ dispatch }) {
+      WeatherApi.getWeather()
         .then(data => {
-          console.log(data.data)
           dispatch("setWeather", data.data);
         })
         .catch(error => {
           console.log(error);
         });
     },
-    async loadWeatherList({ dispatch }) {
-      for (let i = 0; i < 5; i++) {
-        await WeatherApi.getWeather()
-          .then(data => {
-            console.log(data.data)
-            dispatch("setWeather", data.data);
-          })
-          .catch(error => {
-            console.log(error);
-          });
     removeFromLikedHistory({ state, commit }, weather) {
       let weatherFromLikedList = state.likedHistory.indexOf(weather);
       commit("REMOVE_FROM_LIKED_HISTORY", weatherFromLikedList);
@@ -84,9 +80,9 @@ export default new Vuex.Store({
   getters: {
     currentWeather: state => {
       if (state.weatherList.length) {
-        return state.weatherList[0]
+        return state.weatherList[0];
       } else {
-        return false
+        return false;
       }
     }
   }
